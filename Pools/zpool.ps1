079fe8fed3a2a70ea9a1ef709fc54086605a6e93
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2018-12-01 16:22:45 +0100

    Pool Failing #870
    #870

--- Pools/zpool.ps1
+++ Pools/zpool.ps1
@@ -15,47 +15,48 @@ $DivisorMultiplier = 1000000
  
 $Locations = "eu", "us", "jp", "na", "sea"
 $Locations | ForEach-Object {
-    $zpoolplus_Location = $_
-        
-    switch ($zpoolplus_Location) {
-        "eu" {$Location = "eu"} #Europe
-        "us" {$Location = "us"} #United States of America
-        "jp" {$Location = "jp"} #Japan
-        "na" {$Location = "na"} #North America
-        "sea" {$Location = "sea"} #South East Asia
-        default {$Location = "us"}
-    }
-    
-    # Placed here for Perf (Disk reads)
-    $ConfName = if ($Config.PoolsConfig.$Name -ne $Null) {$Name}else {"default"}
-    $PoolConf = $Config.PoolsConfig.$ConfName
+        $zpoolplus_Location = $_
 
-    $Request | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
-        $PoolHost = "$($_).$($Location)$($HostSuffix)"
-        $PoolPort = $Request.$_.port
-        $PoolAlgorithm = Get-Algorithm $Request.$_.name
+        switch ($zpoolplus_Location) {
+            "eu" {$Location = "eu"} #Europe
+            "us" {$Location = "us"} #United States of America
+            "jp" {$Location = "jp"} #Japan
+            "na" {$Location = "na"} #North America
+            "sea" {$Location = "sea"} #South East Asia
+            default {$Location = "us"}
+        }
 
-    $Divisor = $DivisorMultiplier * [Double]$Request.$_.mbtc_mh_factor
+        # Placed here for Perf (Disk reads)
+        $ConfName = if ($Config.PoolsConfig.$Name -ne $Null) {$Name}else {"default"}
+        $PoolConf = $Config.PoolsConfig.$ConfName
 
-    if ((Get-Stat -Name "$($Name)_$($PoolAlgorithm)_Profit") -eq $null) {$Stat = Set-Stat -Name "$($Name)_$($PoolAlgorithm)_Profit" -Value ([Double]$Request.$_.$PriceField / $Divisor * (1 - ($Request.$_.fees / 100)))}
-    else {$Stat = Set-Stat -Name "$($Name)_$($PoolAlgorithm)_Profit" -Value ([Double]$Request.$_.$PriceField / $Divisor * (1 - ($Request.$_.fees / 100)))}
+        $Request | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
+            $PoolHost = "$($_).$($Location)$($HostSuffix)"
+            $PoolPort = $Request.$_.port
+            $PoolAlgorithm = Get-Algorithm $Request.$_.name
 
-    $PwdCurr = if ($PoolConf.PwdCurrency) {$PoolConf.PwdCurrency}else {$Config.Passwordcurrency}
-    $WorkerName = If ($PoolConf.WorkerName -like "ID=*") {$PoolConf.WorkerName} else {"ID=$($PoolConf.WorkerName)"}
+        $Divisor = $DivisorMultiplier * [Double]$Request.$_.mbtc_mh_factor
 
-    if ($PoolConf.Wallet) {
-        [PSCustomObject]@{
-            Algorithm     = $PoolAlgorithm
-            Price         = $Stat.Live*$PoolConf.PricePenaltyFactor
-            StablePrice   = $Stat.Week
-            MarginOfError = $Stat.Week_Fluctuation
-            Protocol      = "stratum+tcp"
-            Host          = $PoolHost
-            Port          = $PoolPort
-            User          = $PoolConf.Wallet
-            Pass          = "$($WorkerName),c=$($PwdCurr)"
-            Location      = $Location
-            SSL           = $false
+        if ((Get-Stat -Name "$($Name)_$($PoolAlgorithm)_Profit") -eq $null) {$Stat = Set-Stat -Name "$($Name)_$($PoolAlgorithm)_Profit" -Value ([Double]$Request.$_.$PriceField / $Divisor * (1 - ($Request.$_.fees / 100)))}
+        else {$Stat = Set-Stat -Name "$($Name)_$($PoolAlgorithm)_Profit" -Value ([Double]$Request.$_.$PriceField / $Divisor * (1 - ($Request.$_.fees / 100)))}
+
+        $PwdCurr = if ($PoolConf.PwdCurrency) {$PoolConf.PwdCurrency}else {$Config.Passwordcurrency}
+        $WorkerName = If ($PoolConf.WorkerName -like "ID=*") {$PoolConf.WorkerName} else {"ID=$($PoolConf.WorkerName)"}
+
+        if ($PoolConf.Wallet) {
+            [PSCustomObject]@{
+                Algorithm     = $PoolAlgorithm
+                Price         = $Stat.Live*$PoolConf.PricePenaltyFactor
+                StablePrice   = $Stat.Week
+                MarginOfError = $Stat.Week_Fluctuation
+                Protocol      = "stratum+tcp"
+                Host          = $PoolHost
+                Port          = $PoolPort
+                User          = $PoolConf.Wallet
+                Pass          = "$($WorkerName),c=$($PwdCurr)"
+                Location      = $Location
+                SSL           = $false
+            }
         }
     }
 }
